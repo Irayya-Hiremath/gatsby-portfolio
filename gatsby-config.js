3cdceeac68a17fc95e6959e2b13f25449029d3ab
irayya <ihiremath1991@gmail.com>
2022-05-10 01:24:53 +0530

    pwa

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,14 +3,37 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `IrayyaHiremath`,
-        short_name: `IrayyaHiremath`,
-        start_url: `/`,
-        background_color: `#363636`,
-        theme_color: `#262626`,
-        display: `standalone`,
-        icon: `/favicon.ico`,
-      },
-    },
+      theme_color: "#262626",
+      background_color: "#363636",
+      display: "standalone",
+      scope: "/",
+      start_url: "/",
+      name: "IrayyaHiremath",
+      short_name: "IrayyaHiremath",
+     description: "Portpolio",
+      icons: [
+          {
+              src: "/icon-192x192.png",
+              sizes: "192x192",
+              type: "image/png"
+          },
+          {
+              src: "/icon-256x256.png",
+              sizes: "256x256",
+              type: "image/png"
+          },
+          {
+              src: "/icon-384x384.png",
+              sizes: "384x384",
+              type: "image/png"
+          },
+          {
+              src: "/icon-512x512.png",
+            sizes: "512x512",
+              type: "image/png"
+          }
+      ]
+  }
+}
   ],
 }
